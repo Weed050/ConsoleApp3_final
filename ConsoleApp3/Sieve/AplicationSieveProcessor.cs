62a3c518b8cc68a6dcf4b3b370e10fd0a30f8aab
Weed050 <84383140+Weed050@users.noreply.github.com>
2023-02-25 13:15:36 +0100

    defined sort and filter for author properties

---
+++ ConsoleApp3/Sieve/AplicationSieveProcessor.cs
@@ -0,0 +1,30 @@
+﻿using ConsoleApp3.Models;
+using Microsoft.Extensions.Options;
+using Sieve.Models;
+using Sieve.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3.Sieve
+{
+    internal class AplicationSieveProcessor : SieveProcessor
+    {
+        public AplicationSieveProcessor(IOptions<SieveOptions> options) : base(options)
+        {
+        }
+        protected override SievePropertyMapper MapProperties(SievePropertyMapper mapper)
+        {
+            mapper.Property<Authors>(e => e.AuthorsID)
+                .CanSort()
+                .CanFilter();
+            mapper.Property<Authors>(e => e.AuthorName)
+              .CanSort()
+              .CanFilter();
+            return mapper;
+
+        }
+    }
+}
